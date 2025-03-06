cf37781adca35f5dc6ad31d428a9eda06e3a7eed
Giulliano <tharsoweb@gmail.com>
2025-03-06 10:49:57 -0300

    project structure

---
+++ src/defs/definitions.js
@@ -0,0 +1,112 @@
+export const mainMenu = [
+    {
+        menu: "Home",
+        url: "#",
+    },
+    {
+        menu: "About",
+        url: "#",
+    },
+    {
+        menu: "Contact",
+        url: "#",
+    },
+    {
+        menu: "Blog",
+        url: "#",
+    },
+    {
+        menu: "Careers",
+        url: "#",
+    },
+]
+
+export const footerMenu = [
+    ...mainMenu,
+    {
+        menu: "Privacy Policy",
+        url: "#"
+    },
+]
+
+export const benefits = [
+    {
+        icon: image => image,
+        title: "Online Banking",
+        text: "Our modern web and mobile applications allow you to keep track of your finances wherever you are in the world."
+    },
+    {
+        icon: image => image,
+        title: "Simple Budgeting",
+        text: "See exactly where your money goes each month. Receive notifications when you’re close to hitting your limits."
+    },
+    {
+        icon: image => image,
+        title: "Fast Onboarding",
+        text: "We don’t do branches. Open your account in minutes online and start taking control of your finances right away."
+    },
+    {
+        icon: image => image,
+        title: "Open API",
+        text: "Manage your savings, investments, pension, and much more from one account. Tracking your money has never been easier."
+    },
+]
+
+export const posts = [
+    {
+        cover: image => image,
+        title: "Receive money in any currency with no fees",
+        description: "The world is getting smaller and we’re becoming more mobile. So why should you be forced to only receive money in a single …",
+        author: "Claire Robinson",
+        url: "#"
+    },
+    {
+        cover: image => image,
+        title: "Treat yourself without worrying about money",
+        description: "Our simple budgeting feature allows you to separate out your spending and set realistic limits each month. That means you …",
+        author: "Wilson Hutton",
+        url: "#"
+    },
+    {
+        cover: image => image,
+        title: "Take your Easybank card wherever you go",
+        description: "We want you to enjoy your travels. This is why we don’t charge any fees on purchases while you’re abroad. We’ll even show you …",
+        author: "Wilson Hutton",
+        url: "#"
+    },
+    {
+        cover: image => image,
+        title: "Our invite-only Beta accounts are now live!",
+        description: "After a lot of hard work by the whole team, we’re excited to launch our closed beta. It’s easy to request an invite through the site ...",
+        author: "Claire Robinson",
+        url: "#"
+    },
+]
+
+export const socialLinks = [
+    {
+        icon: image => image,
+        platform: "Facebook",
+        url: "#"
+    },
+    {
+        icon: image => image,
+        platform: "Youtube",
+        url: "#"
+    },
+    {
+        icon: image => image,
+        platform: "Twitter",
+        url: "#"
+    },
+    {
+        icon: image => image,
+        platform: "Pinterest",
+        url: "#"
+    },
+    {
+        icon: image => image,
+        platform: "Instagram",
+        url: "#"
+    },
+]
